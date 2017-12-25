6a805243d51f8f71f09588983a64449925ec6e82
Nitin <tulswani19@gmail.com>
2017-12-25 13:46:47 +0530

    renamed

--- extension/bootstrap.js
+++
@@ -1,8 +0,0 @@
-chrome.devtools.panels.create(
-  "React Performance",
-  "./icon/RP48.png",
-  "load.html",
-  function(panel) {
-    console.log("Started!");
-  }
-);
---
+++ extension/panel.js
@@ -0,0 +1,8 @@
+chrome.devtools.panels.create(
+  'React Performance',
+  './icon/RP48.png',
+  'load.html',
+  function(panel) {
+    console.log('Started!')
+  }
+)
