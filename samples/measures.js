6132c36e04e351d1d2e991af792d869717ee9828
Nitin <tulswani19@gmail.com>
2017-12-25 13:34:06 +0530

    replaced the old measures

--- samples/measures.js
+++ samples/measures.js
@@ -1,182 +1,182 @@
 const MEASURES = [
   {
     name:
-      "⛔ (React Tree Reconciliation) Warning: There were cascading updates",
-    entryType: "measure",
-    startTime: 449.15500000000003,
-    duration: 84.66500000000002
+      '⛔ (React Tree Reconciliation) Warning: There were cascading updates',
+    entryType: 'measure',
+    startTime: 414.90500000000003,
+    duration: 54.74000000000001
   },
   {
-    name: "⚛ App [mount]",
-    entryType: "measure",
-    startTime: 451.38000000000005,
-    duration: 58.59999999999991
+    name: '⚛ App [mount]',
+    entryType: 'measure',
+    startTime: 417.13500000000005,
+    duration: 36.875
   },
   {
-    name: "⚛ App.componentWillMount",
-    entryType: "measure",
-    startTime: 452.27500000000003,
-    duration: 3.1499999999999773
+    name: '⚛ App.componentWillMount',
+    entryType: 'measure',
+    startTime: 418.055,
+    duration: 3.0249999999999773
   },
   {
-    name: "⚛ Main [mount]",
-    entryType: "measure",
-    startTime: 456.56500000000005,
-    duration: 53.05000000000001
+    name: '⚛ Main [mount]',
+    entryType: 'measure',
+    startTime: 422.55,
+    duration: 31.350000000000023
   },
   {
-    name: "⚛ Main.componentWillMount",
-    entryType: "measure",
-    startTime: 457.02000000000004,
-    duration: 2.1999999999999886
+    name: '⚛ Main.componentWillMount',
+    entryType: 'measure',
+    startTime: 423.15000000000003,
+    duration: 0.875
   },
   {
-    name: "⚛ Introduction [mount]",
-    entryType: "measure",
-    startTime: 462.12,
-    duration: 14.260000000000048
+    name: '⚛ Introduction [mount]',
+    entryType: 'measure',
+    startTime: 425.5450000000001,
+    duration: 11.33499999999998
   },
   {
-    name: "⚛ Introduction.componentWillMount",
-    entryType: "measure",
-    startTime: 463.035,
-    duration: 0.8000000000000114
+    name: '⚛ Introduction.componentWillMount',
+    entryType: 'measure',
+    startTime: 426.005,
+    duration: 0.8350000000000364
   },
   {
-    name: "⚛ Projects [mount]",
-    entryType: "measure",
-    startTime: 476.40500000000003,
-    duration: 17.335000000000036
+    name: '⚛ Projects [mount]',
+    entryType: 'measure',
+    startTime: 436.91,
+    duration: 9.435000000000002
   },
   {
-    name: "⚛ Projects.componentWillMount",
-    entryType: "measure",
-    startTime: 477.88500000000005,
-    duration: 4.044999999999959
+    name: '⚛ Projects.componentWillMount',
+    entryType: 'measure',
+    startTime: 437.595,
+    duration: 1.8450000000000273
   },
   {
-    name: "⚛ Posts [mount]",
-    entryType: "measure",
-    startTime: 493.77500000000003,
-    duration: 6.535000000000025
+    name: '⚛ Posts [mount]',
+    entryType: 'measure',
+    startTime: 446.365,
+    duration: 4.045000000000016
   },
   {
-    name: "⚛ Posts.componentWillMount",
-    entryType: "measure",
-    startTime: 496.40500000000003,
-    duration: 1.6000000000000227
+    name: '⚛ Posts.componentWillMount',
+    entryType: 'measure',
+    startTime: 446.975,
+    duration: 1.1050000000000182
   },
   {
-    name: "⚛ Message [mount]",
-    entryType: "measure",
-    startTime: 500.71,
-    duration: 8.740000000000066
+    name: '⚛ Message [mount]',
+    entryType: 'measure',
+    startTime: 450.43000000000006,
+    duration: 3.4149999999999636
   },
   {
-    name: "⚛ Message.componentWillMount",
-    entryType: "measure",
-    startTime: 503.45000000000005,
-    duration: 1.2250000000000227
-  },
-  {
-    name:
-      "⛔ (Committing Changes) Warning: Lifecycle hook scheduled a cascading update",
-    entryType: "measure",
-    startTime: 510.7250000000001,
-    duration: 10.634999999999934
-  },
-  {
-    name: "⚛ (Committing Host Effects: 7 Total)",
-    entryType: "measure",
-    startTime: 511.28499999999997,
-    duration: 0.7649999999999864
-  },
-  {
-    name: "⚛ (Calling Lifecycle Methods: 6 Total)",
-    entryType: "measure",
-    startTime: 512.2450000000001,
-    duration: 9.014999999999986
+    name: '⚛ Message.componentWillMount',
+    entryType: 'measure',
+    startTime: 450.85,
+    duration: 0.6700000000000159
   },
   {
     name:
-      "⛔ Introduction.componentDidMount Warning: Scheduled a cascading update",
-    entryType: "measure",
-    startTime: 513.345,
-    duration: 0.5099999999999909
+      '⛔ (Committing Changes) Warning: Lifecycle hook scheduled a cascading update',
+    entryType: 'measure',
+    startTime: 454.31000000000006,
+    duration: 6.214999999999975
   },
   {
-    name: "⛔ Projects.componentDidMount Warning: Scheduled a cascading update",
-    entryType: "measure",
-    startTime: 514.59,
-    duration: 0.20000000000004547
+    name: '⚛ (Committing Host Effects: 7 Total)',
+    entryType: 'measure',
+    startTime: 454.61500000000007,
+    duration: 1.1399999999999295
   },
   {
-    name: "⚛ Posts.componentDidMount",
-    entryType: "measure",
-    startTime: 514.82,
-    duration: 2.07000000000005
-  },
-  {
-    name: "⚛ Message.componentDidMount",
-    entryType: "measure",
-    startTime: 517.02,
-    duration: 0.8949999999999818
-  },
-  {
-    name: "⚛ Main.componentDidMount",
-    entryType: "measure",
-    startTime: 517.9449999999999,
-    duration: 1.6650000000000773
-  },
-  {
-    name: "⚛ App.componentDidMount",
-    entryType: "measure",
-    startTime: 519.6800000000001,
-    duration: 1.2199999999999136
-  },
-  {
-    name: "⚛ Introduction [update]",
-    entryType: "measure",
-    startTime: 522.8050000000001,
-    duration: 1.9149999999999636
-  },
-  {
-    name: "⚛ Projects [update]",
-    entryType: "measure",
-    startTime: 524.7400000000001,
-    duration: 7.100000000000023
+    name: '⚛ (Calling Lifecycle Methods: 6 Total)',
+    entryType: 'measure',
+    startTime: 456.03000000000003,
+    duration: 4.3799999999999955
   },
   {
     name:
-      "⛔ (Committing Changes) Warning: Caused by a cascading update in earlier commit",
-    entryType: "measure",
-    startTime: 532.0400000000001,
-    duration: 1.544999999999959
+      '⛔ Introduction.componentDidMount Warning: Scheduled a cascading update',
+    entryType: 'measure',
+    startTime: 456.47,
+    duration: 0.4600000000000364
   },
   {
-    name: "⚛ (Committing Host Effects: 1 Total)",
-    entryType: "measure",
-    startTime: 532.1700000000001,
-    duration: 0.30999999999994543
+    name: '⛔ Projects.componentDidMount Warning: Scheduled a cascading update',
+    entryType: 'measure',
+    startTime: 456.95500000000004,
+    duration: 0.07999999999998408
   },
   {
-    name: "⚛ (Calling Lifecycle Methods: 1 Total)",
-    entryType: "measure",
-    startTime: 532.51,
-    duration: 1.0550000000000637
+    name: '⚛ Posts.componentDidMount',
+    entryType: 'measure',
+    startTime: 457.05,
+    duration: 0.8250000000000455
   },
   {
-    name: "⚛ Introduction.componentDidUpdate",
-    entryType: "measure",
-    startTime: 532.57,
-    duration: 0.849999999999909
+    name: '⚛ Message.componentDidMount',
+    entryType: 'measure',
+    startTime: 457.9200000000001,
+    duration: 0.55499999999995
   },
   {
-    name: "⚛ (React Tree Reconciliation)",
-    entryType: "measure",
-    startTime: 773.205,
-    duration: 0.13999999999998636
+    name: '⚛ Main.componentDidMount',
+    entryType: 'measure',
+    startTime: 458.49500000000006,
+    duration: 0.8099999999999454
+  },
+  {
+    name: '⚛ App.componentDidMount',
+    entryType: 'measure',
+    startTime: 459.33000000000004,
+    duration: 0.9200000000000159
+  },
+  {
+    name: '⚛ Introduction [update]',
+    entryType: 'measure',
+    startTime: 462.17500000000007,
+    duration: 2.8349999999999795
+  },
+  {
+    name: '⚛ Projects [update]',
+    entryType: 'measure',
+    startTime: 465.035,
+    duration: 3.025000000000034
+  },
+  {
+    name:
+      '⛔ (Committing Changes) Warning: Caused by a cascading update in earlier commit',
+    entryType: 'measure',
+    startTime: 468.31000000000006,
+    duration: 1.1399999999999864
+  },
+  {
+    name: '⚛ (Committing Host Effects: 1 Total)',
+    entryType: 'measure',
+    startTime: 468.425,
+    duration: 0.08999999999997499
+  },
+  {
+    name: '⚛ (Calling Lifecycle Methods: 1 Total)',
+    entryType: 'measure',
+    startTime: 468.535,
+    duration: 0.8899999999999864
+  },
+  {
+    name: '⚛ Introduction.componentDidUpdate',
+    entryType: 'measure',
+    startTime: 468.57500000000005,
+    duration: 0.8100000000000023
+  },
+  {
+    name: '⚛ (React Tree Reconciliation)',
+    entryType: 'measure',
+    startTime: 634.6050000000001,
+    duration: 0.15999999999985448
   }
-];
+]
 
-module.exports = MEASURES;
+module.exports = MEASURES
