fc91a7c176282bffc3f92a06b657de5323e2f8cf
Nitin <tulswani19@gmail.com>
2017-12-29 18:15:18 +0530

    Added samples for parsed measures

---
+++ samples/parsed.js
@@ -0,0 +1,370 @@
+const MEASURES = [
+  {
+    componentName: 'App',
+    totalTimeSpent: 32.48,
+    numberOfInstances: 1,
+    percentTimeSpent: '33%',
+    render: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    mount: {
+      averageTimeSpentMs: '32.48',
+      numberOfTimes: 1,
+      totalTimeSpentMs: 32.48
+    },
+    update: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    unmount: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    componentWillMount: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    componentDidMount: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    componentWillReceiveProps: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    shouldComponentUpdate: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    componentWillUpdate: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    componentDidUpdate: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    componentWillUnmount: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    }
+  },
+  {
+    componentName: 'Main',
+    totalTimeSpent: 30.56,
+    numberOfInstances: 1,
+    percentTimeSpent: '31%',
+    render: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    mount: {
+      averageTimeSpentMs: '30.56',
+      numberOfTimes: 1,
+      totalTimeSpentMs: 30.56
+    },
+    update: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    unmount: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    componentWillMount: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    componentDidMount: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    componentWillReceiveProps: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    shouldComponentUpdate: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    componentWillUpdate: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    componentDidUpdate: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    componentWillUnmount: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    }
+  },
+  {
+    componentName: 'Projects',
+    totalTimeSpent: 18.87,
+    numberOfInstances: 1,
+    percentTimeSpent: '19%',
+    render: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    mount: {
+      averageTimeSpentMs: '14.89',
+      numberOfTimes: 1,
+      totalTimeSpentMs: 14.89
+    },
+    update: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    unmount: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    componentWillMount: {
+      averageTimeSpentMs: '3.98',
+      numberOfTimes: 1,
+      totalTimeSpentMs: 3.98
+    },
+    componentDidMount: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    componentWillReceiveProps: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    shouldComponentUpdate: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    componentWillUpdate: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    componentDidUpdate: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    componentWillUnmount: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    }
+  },
+  {
+    componentName: 'Introduction',
+    totalTimeSpent: 10.1,
+    numberOfInstances: 1,
+    percentTimeSpent: '10%',
+    render: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    mount: {
+      averageTimeSpentMs: '7.25',
+      numberOfTimes: 1,
+      totalTimeSpentMs: 7.25
+    },
+    update: {
+      averageTimeSpentMs: '2.25',
+      numberOfTimes: 1,
+      totalTimeSpentMs: 2.25
+    },
+    unmount: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    componentWillMount: {
+      averageTimeSpentMs: '0.17',
+      numberOfTimes: 1,
+      totalTimeSpentMs: 0.17
+    },
+    componentDidMount: {
+      averageTimeSpentMs: '0.43',
+      numberOfTimes: 1,
+      totalTimeSpentMs: 0.43
+    },
+    componentWillReceiveProps: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    shouldComponentUpdate: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    componentWillUpdate: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    componentDidUpdate: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    componentWillUnmount: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    }
+  },
+  {
+    componentName: 'Message',
+    totalTimeSpent: 3.59,
+    numberOfInstances: 1,
+    percentTimeSpent: '4%',
+    render: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    mount: {
+      averageTimeSpentMs: '3.59',
+      numberOfTimes: 1,
+      totalTimeSpentMs: 3.59
+    },
+    update: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    unmount: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    componentWillMount: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    componentDidMount: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    componentWillReceiveProps: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    shouldComponentUpdate: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    componentWillUpdate: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    componentDidUpdate: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    componentWillUnmount: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    }
+  },
+  {
+    componentName: 'Posts',
+    totalTimeSpent: 3.58,
+    numberOfInstances: 1,
+    percentTimeSpent: '4%',
+    render: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    mount: {
+      averageTimeSpentMs: '3.58',
+      numberOfTimes: 1,
+      totalTimeSpentMs: 3.58
+    },
+    update: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    unmount: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    componentWillMount: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    componentDidMount: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    componentWillReceiveProps: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    shouldComponentUpdate: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    componentWillUpdate: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    componentDidUpdate: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    },
+    componentWillUnmount: {
+      averageTimeSpentMs: '-',
+      numberOfTimes: 0,
+      totalTimeSpentMs: 0
+    }
+  }
+]
+
+module.exports = MEASURES
