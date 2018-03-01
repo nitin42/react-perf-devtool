7e0fda76a19c35bfde7941b76eb68aa191c905ec
Nitin <tulswani19@gmail.com>
2018-03-01 22:29:11 +0530

    improved error messages

--- src/extension/components/ErrorComponent.js
+++ src/extension/components/ErrorComponent.js
@@ -3,23 +3,26 @@ import React from 'react'
 export function ErrorComponent() {
   return (
     <div style={{ fontWeight: 500, padding: '8px' }}>
-      An error occurred while collecting the measures. To resolve this error,
-      try these solutions:
+      An error occurred while collecting the measures. This is possibly due to
       <ul>
         <li>
-          Make sure you have registered a top level observer in your project.{' '}
+          absence of register observer hook in your project.{' '}
           <a
             href="https://github.com/nitin42/react-perf-devtool/tree/v2#using-the-browser-extension"
             style={{ textDecoration: 'none' }}
+            target="_blank"
           >
             See the detailed documentation
           </a>{' '}
-          on how to register a top level listener (an observer) in your React
-          project app.
+          on how to register a top level observer in your React application.
         </li>
         <br />
-        <li>Refresh the the page or reload the inspected window.</li>
+        <li>your project is not using React.</li>
       </ul>
+      <p>
+        If above solutions don't work, then try reloading the plugin or close
+        and reopen the inspected window.
+      </p>
     </div>
   )
 }
