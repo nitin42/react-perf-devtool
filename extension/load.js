5ec62c9277c3a259215a20c41af115065c34d325
Nitin <tulswani19@gmail.com>
2018-03-01 22:28:22 +0530

    link Chart.js

--- extension/load.js
+++ extension/load.js
@@ -1,15 +1,19 @@
 require.config({
   paths: {
     react: './dependencies/react',
-    'react-dom': './dependencies/react-dom'
+    'react-dom': './dependencies/react-dom',
+    Chart: './dependencies/Chart'
   }
 })
 
-requirejs(['react', 'react-dom', '../build/ReactPerfDevtool'], function(
-  React,
-  ReactDOM,
-  { ReactPerfDevtool }
-) {
-  const root = document.getElementById('root')
-  ReactDOM.render(React.createElement(ReactPerfDevtool), root)
-})
+requirejs(
+  ['react', 'react-dom', 'Chart', '../build/ReactPerfDevtool'],
+  function(React, ReactDOM, Chart, { ReactPerfDevtool }) {
+    const root = document.getElementById('root')
+
+    ReactDOM.render(
+      React.createElement(ReactPerfDevtool, { Graphics: Chart }),
+      root
+    )
+  }
+)
